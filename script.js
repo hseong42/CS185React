a65cc38063b3c0043ca55439e143dd4db898e138
Hyewon Seong <hseong@umail.ucsb.edu>
2020-04-20 03:36:28 -0700

    Changed scroll function

--- script.js
+++ script.js
@@ -43,7 +43,11 @@ function scroll() {
 }
 
 
+
 function toTop() {
-	document.body.scrollTop = 0
-    document.documentElement.scrollTop = 0
+	const btn = document.querySelector("#top")
+	btn.addEventListener("click", event => {
+		document.body.scrollTop = 0
+    	document.documentElement.scrollTop = 0
+	})
 }
