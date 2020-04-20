7e252fc2fcbbecdd417e06afbe16bb87f57d767f
Hyewon Seong <hseong@umail.ucsb.edu>
2020-04-20 03:02:18 -0700

    Tentative HW3 submission

---
+++ script.js
@@ -0,0 +1,49 @@
+function load() {
+	window.addEventListener("scroll", scroll)
+
+	$(".img").on("click", function(click){
+        $("#imagesModal").show()
+        click.stopPropagation()
+    })
+
+	$("body").click(function() {
+    })
+
+    $("#imagesModal").click(function() {
+    	$("#imagesModal").hide()
+    })
+
+    $("#image").click(function(click) {
+        click.stopPropagation()
+    })
+
+}
+
+function bigPicture(image) {
+	var modalImage = document.getElementById('image')
+	modalImage.src = image
+	document.getElementById('imagesModal').style.display="block"
+}
+
+function closePicture() {
+	document.getElementById('imagesModal').style.display="none"
+}
+
+function keep(click) {
+	click.stopPropogation()
+}
+
+function scroll() {
+	if(document.body.scrollTop > 30 || document.documentElement.scrollTop > 20) {
+		toTop = document.getElementById("toTop")
+		toTop.style.display = "block"
+	} else {
+		toTop.style.display = "none"
+	}
+}
+
+
+function toTop() {
+	document.body.scrollTop = 0
+    document.documentElement.scrollTop = 0
+}
