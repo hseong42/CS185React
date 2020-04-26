4cf797605fc0346a684868d5a5b8e56fe4a5c2e9
Hyewon Seong <hseong@umail.ucsb.edu>
2020-04-25 19:06:59 -0700

    Updates

---
+++ precache-manifest.44d9b46af59c10358d917284607e3821.js
@@ -0,0 +1,102 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9bd3c9873231ee589b39a10f10b3bd5b",
+    "url": "/CS185/index.html"
+  },
+  {
+    "revision": "e0f815c6356e0d0f4027",
+    "url": "/CS185/static/css/main.30b5dab3.chunk.css"
+  },
+  {
+    "revision": "55b67abddb7b32a38540",
+    "url": "/CS185/static/js/2.58a26f3e.chunk.js"
+  },
+  {
+    "revision": "04007fcd50e895bcbb2b21327be92c6e",
+    "url": "/CS185/static/js/2.58a26f3e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e0f815c6356e0d0f4027",
+    "url": "/CS185/static/js/main.7fb7aa96.chunk.js"
+  },
+  {
+    "revision": "de216646813784be4f67",
+    "url": "/CS185/static/js/runtime-main.0ac0af07.js"
+  },
+  {
+    "revision": "852370774601711bc3f2cfa464798956",
+    "url": "/CS185/static/media/A360.85237077.png"
+  },
+  {
+    "revision": "d90b16d51c1e77bc3cb07de275c4e458",
+    "url": "/CS185/static/media/AT2.d90b16d5.jpg"
+  },
+  {
+    "revision": "b7b3362ac5654fa0e85cf44c09835469",
+    "url": "/CS185/static/media/ATLogo.b7b3362a.png"
+  },
+  {
+    "revision": "2e91f9287f53d7c41c1980ed33df23d4",
+    "url": "/CS185/static/media/At1.2e91f928.jpg"
+  },
+  {
+    "revision": "8cf0fd42e2866e993608eeb4eeb8341f",
+    "url": "/CS185/static/media/CGtA.8cf0fd42.png"
+  },
+  {
+    "revision": "39006c6622ca2b36455c0c09ed572ba3",
+    "url": "/CS185/static/media/HoytLogo.39006c66.jpg"
+  },
+  {
+    "revision": "1196df3d56f7401c4a71e9e89a1bf742",
+    "url": "/CS185/static/media/JOAD.1196df3d.jpg"
+  },
+  {
+    "revision": "adc52a67a5ba230fccb48e547e88c681",
+    "url": "/CS185/static/media/PAlogo.adc52a67.png"
+  },
+  {
+    "revision": "ec351d1b204608cc0b2414c8e0cbb62b",
+    "url": "/CS185/static/media/arrowPull.ec351d1b.jpg"
+  },
+  {
+    "revision": "c97064dea8042ed1e81286e7bea7e7cb",
+    "url": "/CS185/static/media/atLine.c97064de.jpg"
+  },
+  {
+    "revision": "01eb7dd23d76c19f0ee0de454eb9b08d",
+    "url": "/CS185/static/media/field.01eb7dd2.jpg"
+  },
+  {
+    "revision": "2f18ed15366d8f92b8c622caf4d1622c",
+    "url": "/CS185/static/media/outdoorRange.2f18ed15.jpg"
+  },
+  {
+    "revision": "60956dcd72a25b50b96d8bd0b1dafdf4",
+    "url": "/CS185/static/media/shoot.60956dcd.jpg"
+  },
+  {
+    "revision": "92feff9825ae9151d3d841577ba6ff04",
+    "url": "/CS185/static/media/shoot.92feff98.mp4"
+  },
+  {
+    "revision": "6ca699256b4e73c5a5f78d3752745de5",
+    "url": "/CS185/static/media/shoot2.6ca69925.jpg"
+  },
+  {
+    "revision": "5d86673a6f6a3a5a566f25b2c199c61b",
+    "url": "/CS185/static/media/shooting.5d86673a.mp4"
+  },
+  {
+    "revision": "409d1f80b74bdcc7510468c8962bfb1e",
+    "url": "/CS185/static/media/target.409d1f80.jpg"
+  },
+  {
+    "revision": "1e1a25b458f3706ad35511385f15d4a2",
+    "url": "/CS185/static/media/targets.1e1a25b4.jpg"
+  },
+  {
+    "revision": "99ae3969126c641ae2e57f5b58085677",
+    "url": "/CS185/static/media/waterBalloon.99ae3969.mp4"
+  }
+]);
